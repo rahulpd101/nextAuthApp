4bd9e786e402818c4f2d882c69e4eb15a7ecaa54
Rahul Prasad <rahulprasad@192.168.1.15>
2024-08-11 14:42:00 +0530

    added two-factor token creation function

--- lib/tokens.ts
+++ lib/tokens.ts
@@ -2,6 +2,8 @@ import { v4 as uuidv4 } from "uuid";
 import { getVerificationTokenByEmail } from "@/data/verification-token";
 import { db } from "@/lib/db";
 import { getPasswordResetTokenByEmail } from "@/data/password-reset-token";
+import crypto from "crypto";
+import { getTwoFactorTokenByEmail } from "@/data/two-factor-token";
 
 export const generatePasswordResetToken = async (email: string) => {
   const token = uuidv4();
@@ -45,4 +47,27 @@ export const generateVerificationToken = async (email: string) => {
   });
 
   return verificationToken;
+};
+
+export const generateTwoFactorToken = async (email: string) => {
+  const token = crypto.randomInt(100_000, 1_000_000).toString();
+  // current expiring time is 1 hour
+  const expires = new Date(new Date().getTime() + 3600 * 1000);
+
+  const existingToken = await getTwoFactorTokenByEmail(email);
+  if (existingToken) {
+    db.twoFactorToken.delete({
+      where: { id: existingToken.id },
+    });
+  }
+
+  const twoFactorToken = await db.twoFactorToken.create({
+    data: {
+      email,
+      expires,
+      token,
+    },
+  });
+
+  return twoFactorToken;
 };

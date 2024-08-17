d1493ff46bd83b7bd8958f7571b68035a68f389a
Rahul Prasad <rpsp981@gmail.com>
2024-08-17 15:38:03 +0530

    settings page navbar implementation

---
+++ app/(protected)/layout.tsx
@@ -0,0 +1,16 @@
+import { Navabar } from "./_components/navbar";
+
+interface ProtectedLayoutProps {
+  children: React.ReactNode;
+}
+
+const ProtectedLayout = ({ children }: ProtectedLayoutProps) => {
+  return (
+    <div className="min-h-dvh flex flex-col gap-y-10 items-center justify-center bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-sky-400 to-blue-800">
+      <Navabar />
+      {children}
+    </div>
+  );
+};
+
+export default ProtectedLayout;
--- app/(protected)/settings/page.tsx
+++ app/(protected)/settings/page.tsx
@@ -1,20 +1,20 @@
-import { auth, signOut } from "@/auth";
-import { Button } from "@/components/ui/button";
+"use client";
 
-const SettingsPage = async () => {
-  const session = await auth();
+import { logout } from "@/actions/logout";
+import { Button } from "@/components/ui/button";
+import { currentUser } from "@/hooks/use-current-user";
+
+const SettingsPage = () => {
+  const session = currentUser();
+  const signOutClick = () => {
+    logout();
+  };
   return (
-    <div className="flex flex-col p-8 items-center justify-center">
-      {JSON.stringify(session)}
-      <form
-        action={async () => {
-          "use server";
-          await signOut();
-        }}
-      >
-        <Button className="mt-8" type="submit">
+    <div className="bg-white p-10 rounded-xl">
+      <form>
+        <button onClick={signOutClick} type="submit">
           Sign out
-        </Button>
+        </button>
       </form>
     </div>
   );
